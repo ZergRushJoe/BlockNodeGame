40d8ccdff2f18f648e26d4fdac0dbebb47425c62
Joseph Karp <joe12345hd@yahoo.com>
2017-07-27 10:45:16 -0400

    add test to grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,9 @@
+//const path = require('path');
+
+module.exports = function(grunt)
+{
+    grunt.registerTask('default', 'Log some stuff.', function() {
+    grunt.log.write('Logging some stuff...').ok();
+  });
+
+};
