e3ef80420c176a3cced1ba1570c51486536fa109
Joseph Karp <joe12345hd@yahoo.com>
2017-07-28 16:07:05 -0400

    making gen function

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,31 +8,49 @@ module.exports = function(grunt)
         grunt.log.writeln('start');
         //run async tasks
         let done = this.async();
-        const globby = require('globby');
-        const concat = require('concat');
-        const sass = require('node-sass');
-        const CleanCSS = require('clean-css');
-        const fs = require('fs');
-        globby(['./scr/sass/*'])
-        .then((paths)=>
+        for(let x of scssMinTask(done))
         {
-            concat(paths).then((result)=>
-            {
-                let complied = sass.renderSync({data: result});
-                let ouput = new CleanCSS({}).minify(complied.css)
-                fs.writeFile('./public/stylesheets/style.css', ouput.styles,
-                function (err)
-                {
-                    if (err) {grunt.log.writeln(err);done();};
-                    done();
-                });
-            });
-        })
-        .catch((err)=>
-        {
-            grunt.log.writeln(err);
-            done();
-        });
+            grunt.log.writeln(JSON.stringify(x));
+        }
     });
 
 };
+function* scssMinTask(done)
+{
+    const globby = require('globby');
+    const concat = require('concat');
+    const sass = require('node-sass');
+    const CleanCSS = require('clean-css');
+    const fs = require('fs');
+    let concated,complied,output,inputPaths;
+
+    yield inputPaths = globby(['./scr/sass/*'])
+    .then((paths)=>{return paths})
+    .catch((err)=>
+    {
+        obj = {error:err,subtask:'globby'};
+        return obj;
+    });
+    yield concated = concat(inputPaths)
+    .then((result)=>{return result;})
+    .catch((err) =>
+    {
+        obj = {error:err,subtask:'concat sass'};
+        return obj;
+    });
+
+    yield complied = sass.renderSync({data: concated});
+    yield output = new CleanCSS({}).minify(complied.css);
+
+    fs.writeFile('./public/stylesheets/style.css', ouput.styles,
+    function (err)
+    {
+        if (err)
+        {
+            obj = {error:err,subtask:'fs'};
+            return obj;
+        };
+        done();
+    });
+
+}
